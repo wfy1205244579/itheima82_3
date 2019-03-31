378671989dc8aa27cc68d9ae1dcf93e799c848da
wangfeiyang email=wfy1205244579@163.com <wfy1205244579@163.com>
2019-03-31 10:38:44 +0800

    this is a first commit

--- Mybatis_day01_homework/src/main/java/com/itheima/domain/User.java
+++ Mybatis_day01_homework/src/main/java/com/itheima/domain/User.java
@@ -3,6 +3,9 @@ package com.itheima.domain;
 import java.util.Date;
 
 public class User {
+    public static void main(String[] args) {
+        System.out.println("这是A第一次上传的代码");
+    }
 
     private Integer id;
     private String username;
